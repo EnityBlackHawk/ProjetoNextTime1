39eb36de88599c6627a6f210136e009578d0095e
João Pedro <jcervinobarbosa@gmail.com>
2023-08-22 16:03:20 -0300

    Revert "Merge branch 'dev' into links/anchors"
    This reverts commit 5c017d618ad295bc4e184b6f1490c14d03a08088, reversing
    changes made to f0e85f5d2754575094a2bf1d9fece0c947f4b2e4.

--- src/pages/components/header/index.jsx
+++ src/pages/components/header/index.jsx
@@ -3,47 +3,70 @@ import Link from "next/link";
 import Logo from "/public/images/logo.svg";
 import Styles from "./header.module.scss";
 import Button from "./botton";
-import menuIcon from "/public/icons/menu-outline.svg";
-import { useEffect, useState } from "react";
+import menuIcon from "/public/icons/menu-outline.svg"
+import {useEffect, useState} from "react"
 
-var isOpen = false;
-var inMobile = false;
+var isExpanded = true
+var inMobile = false
 
-function closeMenu() {
-  console.log("close");
-  menu.style = [];
-  isOpen = false;
+function onSizeChanged()
+{
+    let menu = document.getElementById("menu")
+    let header = document.getElementById("header")
+    if(header.offsetWidth <= 731 && isExpanded)
+    {
+        toogleMenu()
+    }
+    else if((header.offsetWidth >= 731) && !isExpanded)
+    {
+        menu.classList.remove(Styles.hidden)
+        menu.classList.remove(Styles.expanded)
+        isExpanded = false
+    }
 }
 
-function toggleMenu() {
-  var menu = document.getElementById("menu");
-  
-  if (isOpen) {
-    closeMenu();
-  } else {
-    menu.style.display = "flex";
-    isOpen = true;
-  }
+function toogleMenu() {
+    let header = document.getElementById("header")
+    let menu = document.getElementById("menu")
+    if(!isExpanded)
+    {
+        menu.classList.add(Styles.expanded)
+        menu.classList.remove(Styles.hidden)
+        isExpanded = true
+    }
+    else
+    {
+        menu.classList.add(Styles.hidden)
+        menu.classList.remove(Styles.expanded)
+        isExpanded = false
+    }    
 }
 
 const Header = () => {
-  return (
-    <div className={Styles.main} id="main">
-      <div className={Styles.container}>
-        <div className={Styles.logotipo}>
-          <Image src={Logo} />
-        </div>
-        <div className={Styles.menu} id="menu">
+    
+    useEffect(() => {
+        window.addEventListener("resize", onSizeChanged)
+        window.addEventListener
+        onSizeChanged()
+    })    
+
+
+    return (
+        <div className={Styles.main} id="header">
+            <div className={Styles.container}>
+                <div className={Styles.logotipo}>
+                    <Image src={Logo} />
+                </div>
+                <div className={Styles.menu} id="menu">
 
                     <Link href="https://google.com">Home</Link>
                     <Link href="https://google.com">O que fazemos</Link>
                     <Link href="https://google.com">Cases</Link>
                     <Button title="Fale conosco" onClick={() => { location.href = "#form" }}/>
                 </div>
+                <Image className={Styles.icon} src={menuIcon} onClick={toogleMenu}/>
+            </div >
         </div>
-        <Image className={Styles.icon} src={menuIcon} onClick={toggleMenu} />
-      </div>
-    </div>
-  );
-};
-export default Header;
+    )
+}
+export default Header
