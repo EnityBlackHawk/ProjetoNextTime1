fd4137f3dcd8adee89e556941c7d90368e34ecc7
João Pedro <101291672+CervinoB@users.noreply.github.com>
2023-08-22 15:55:53 -0300

    fix: change the hrefs to google.com (#28)

--- src/pages/components/header/index.jsx
+++ src/pages/components/header/index.jsx
@@ -34,10 +34,12 @@ const Header = () => {
           <Image src={Logo} />
         </div>
         <div className={Styles.menu} id="menu">
-          <Link href="https://google.com">Home</Link>
-          <Link href="/">O que fazemos</Link>
-          <Link href="/">Cases</Link>
-          <Button title="Fale conosco" />
+
+                    <Link href="https://google.com">Home</Link>
+                    <Link href="https://google.com">O que fazemos</Link>
+                    <Link href="https://google.com">Cases</Link>
+                    <Button title="Fale conosco" onClick={() => { location.href = "#form" }}/>
+                </div>
         </div>
         <Image className={Styles.icon} src={menuIcon} onClick={toggleMenu} />
       </div>
